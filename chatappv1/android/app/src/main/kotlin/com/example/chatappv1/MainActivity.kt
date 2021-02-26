07bc6d6619fcf01e8d80e54860442566dcda9d49
Sahan <shnkalpana@gmail.com>
2021-02-26 06:54:34 +0530

    removed unrelated files contained in the folder

--- chatappv1/android/app/src/main/kotlin/com/example/chatappv1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.chatappv1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- learn_py/li_v1.py
+++
@@ -1,6 +0,0 @@
-num1 = 15
-num2 = 16
-
-sum = num1 + num2
-
-print(sum)
